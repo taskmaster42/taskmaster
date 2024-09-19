0cfc9bd6242df844052020fd7d2887bd9feff5bf
Whenever <joachim.padovani@gmail.com>
2024-09-19 18:57:18 +0200

    feat: add args in main

--- taskmasterctl/main.py
+++ taskmasterctl/main.py
@@ -1,9 +1,14 @@
-from TaskMasterCmd import TaskMasterCmd
+from TaskMasterCtl import TaskMasterCtl
+import sys
 
 
 def main():
-    p = TaskMasterCmd()
-    p.cmdloop()
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <port>")
+        sys.exit(1)
+
+    taskmaster = TaskMasterCtl(int(sys.argv[1]))
+    taskmaster.run()
 
 
 if __name__ == '__main__':
