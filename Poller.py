6c7ea570672f695187ae3561000044a9863ca760
ggay <ggay@student.42.fr>
2024-09-19 18:14:59 +0200

    REFACTO: Poller class changed

--- Poller.py
+++ Poller.py
@@ -10,14 +10,16 @@ class Poller():
 
     def register_process(self, process):
         stdout, stderr = process.get_fd()
-        self.process_registered[stdout] = process
-        self.process_registered[stderr] = process
+        self.process_registered[stdout] = process.get_name()
+        self.process_registered[stderr] = process.get_name()
         self._poll.register(stdout, select.EPOLLIN)
         self._poll.register(stderr, select.EPOLLIN)
 
     def remove_process(self, process):
         stdout, stderr = process.get_fd()
+
         del self.process_registered[stdout]
+
         del self.process_registered[stderr]
         self._poll.unregister(stdout)
         self._poll.unregister(stderr)
@@ -26,8 +28,17 @@ class Poller():
         fd_ready = self._poll.poll(self.timeout)
         if len(fd_ready) == 0:
             return []
-        process_ready = []
+        process_ready = {}
         for fd in fd_ready:
-            process_ready.append(self.process_registered[fd[0]])
-            self.process_registered[fd[0]].set_fd_ready(fd[0])
+            if self.process_registered[fd[0]] in process_ready:
+                process_ready[self.process_registered[fd[0]]].append(fd[0])
+            else:
+                 process_ready[self.process_registered[fd[0]]] = [fd[0]]
+            # self.process_registered[fd[0]].set_fd_ready(fd[0])
         return process_ready
+
+
+    def clear(self):
+        for key , process in self.process_registered.items():
+            self._poll.unregister(key)
+        self.process_registered = {}
